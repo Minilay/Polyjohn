eb9a31a66eec81e4a55f1db13a6f8922bf197fac
Max <minilie88@gmail.com>
2020-10-11 00:54:56 +0600

    Primitive Controll system

--- Assets/_Scripts/Controller.cs
+++ Assets/_Scripts/Controller.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,27 +10,53 @@ public class Controller : MonoBehaviour
     [Header("Inspector")]
     public Animator anim;
     public float speed;
+    public float jumpPower;
     public Rigidbody2D rigid;
 
     [Header("Dynamic")]
     public float velo;
     public bool Hit;
     public bool Shield;
+    public int onGround;
+    public bool jumped;
+    public Vector2 dir;
+    public List<GameObject> touchingObjs;
     void Start()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        touchingObjs.Add(collision.gameObject);
+        if(collision.CompareTag("Ground"))
+        {
+            onGround++;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        touchingObjs.Remove(collision.gameObject);
+        if(collision.CompareTag("Ground"))
+        {
+            onGround--;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         velo = Input.GetAxisRaw("Horizontal") * speed;
-        rigid.velocity = Vector2.right * velo * Time.deltaTime;
+        dir = new Vector2(velo * Time.deltaTime, transform.position.y);
+        
 
-        //rigid.velocity += Vector2.right * velo * Time.deltaTime;
-        transform.localScale = new Vector2(velo>=0 ? 1: -1, 1);
+        rigid.velocity = new Vector2(velo * Time.deltaTime, rigid.velocity.y);
 
+
+        transform.localScale = new Vector2(velo >= 0 ? 1 : -1, 1);
         anim.SetFloat("Speed", Mathf.Abs(velo));
+
+
     }
     private void Update()
     {
@@ -37,5 +65,13 @@ public class Controller : MonoBehaviour
 
         Shield = Input.GetMouseButton(1);
         anim.SetBool("Shield", Shield);
+
+        if(Input.GetButtonDown("Jump") && onGround > 0)
+        {
+            rigid.AddForce(Vector2.up * jumpPower);
+            rigid.velocity = new Vector2(rigid.velocity.x, 0);
+        }
+
+
     }
 }
